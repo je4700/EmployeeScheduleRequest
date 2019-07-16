2148cc9b523d398d89ce6a31de4423f10072a6c1
Jeff Elton <je4700@gmail.com>
2019-07-16 12:29:50 -0400

    Initial commit for Employee Calendar Request

---
+++ EmployeeScheduleRequest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EmployeeScheduleRequest
+//
+//  Created by Jeffery Elton on 7/16/19.
+//  Copyright © 2019 Jeffery Elton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
